35f3b28b217255b6a9f1c698fc9ac8f968c1bb11
Jérémie Simard <jeremie.simard.1@ulaval.ca>
2019-12-07 10:09:45 -0500

    Positions murs

--- quoridorx.py
+++ quoridorx.py
@@ -31,25 +31,52 @@ class Quoridorx(quoridor.Quoridor):
         for x in range(1, 9):
             Quoridorx.dessin(self, (225 - x*50, -225), (225 - x*50, 225))
         Quoridorx.afficher(self)
+        Quoridorx.afficher(self)
 
     def afficher(self):
-        print(self.jeu['joueurs'][0])
-        self.joueur1.goto(Quoridorx.scaling(self.jeu['joueurs'][0]["pos"]))
-        self.joueur2.goto(Quoridorx.scaling(self.jeu['joueurs'][1]["pos"]))
+        # placement joueurs
+        self.joueur1.goto(Quoridorx.scalingj(self.jeu['joueurs'][0]["pos"]))
+        self.joueur2.goto(Quoridorx.scalingj(self.jeu['joueurs'][1]["pos"]))
+        # mur vertical
+        self.dessin.pencolor("red")
+        self.dessin.pensize(5)
+        self.dessin.seth(90)
+        for x in self.jeu["murs"]["verticaux"]:
+            self.dessin.penup()
+            self.dessin.goto(Quoridorx.scalingm(x))
+            self.dessin.pendown()
+            self.dessin.forward(100)
+        # mur horizontal
+        self.dessin.seth(0)
+        for x in self.jeu["murs"]["horizontaux"]:
+            self.dessin.penup()
+            self.dessin.goto(Quoridorx.scalingm(x))
+            self.dessin.pendown()
+            self.dessin.forward(100)
+        
+
     def dessin(self, depart, arrive):
         self.dessin.penup()
         self.dessin.goto(depart)
         self.dessin.pendown()
         self.dessin.goto(arrive)
     
-    def scaling(pos):
+    def scalingj(pos):
         a = ()
         for x in pos:
             a += (x * 50 - 250 ,)
         return a
+    
+    def scalingm(pos):
+        a = ()
+        for x in pos:
+            a += (x * 50 - 275 ,)
+        return a
+    
 
 
 
-TEST1 = Quoridorx([{'nom': 'nom1', 'murs': 10, 'pos': (5, 6)}, {'nom': 'nom2', 'murs': 10, 'pos': (5, 7)}], {'horizontaux': [],'verticaux': []})
+
+TEST1 = Quoridorx([{'nom': 'nom1', 'murs': 10, 'pos': (5, 6)}, {'nom': 'nom2', 'murs': 8, 'pos': (5, 7)}], {'horizontaux': [[4,4]],'verticaux': [[4,4]]})
 
 turtle.mainloop()
