3cdd85bad2278b598b04ce6c2f3f0d7b666c16a4
Jérémie Simard <jeremie.simard.1@ulaval.ca>
2019-12-16 21:15:08 -0500

    pep8

--- quoridor.py
+++ quoridor.py
@@ -150,6 +150,8 @@ class Quoridor:
         return self.jeu
     def final_jouer_coup(self, joueur, joueur2, graphe, pos1, pos2):
         """Méthode qui exécute la fin de la méthode jouer_coup"""
+        # Si on ne peut pas placer de mur on deplace
+        résultat = self.déplacer_jeton(joueur, nx.shortest_path(graphe, pos1, f'B{joueur}')[1])
         # Si shortest path est en x = murh et le pion 2 n'est pas resté au point de départ
         if nx.shortest_path(graphe, pos2, f'B{joueur2}')[1][0] == pos2[0]:
             # Mur h devant le joueur si deplacement vers  l'objectif
@@ -158,7 +160,6 @@ class Quoridor:
                                                                 zip(pos2,
                                                                     (0, joueur - 1)))),
                                             'horizontaux')
-                return résultat
             # mur h a gauche
             except QuoridorError:
                 try:
@@ -166,7 +167,6 @@ class Quoridor:
                                                                     zip(pos2,
                                                                         (-1, joueur - 1)))),
                                                 'horizontaux')
-                    return résultat
                 except QuoridorError:
                     pass
         # Si shortest path en y et au moin 2 murs placés
@@ -178,7 +178,6 @@ class Quoridor:
                                                                  zip(pos2,
                                                                      (0, -1 * (2 - joueur2))))),
                                                'verticaux')
-                    return résultat
                 # Si on ne peut pas placer de mur à cet  endroit, on deplace notre pion
                 except QuoridorError:
                     try:
@@ -186,7 +185,6 @@ class Quoridor:
                                                                      zip(pos2,
                                                                          (0, 1 - joueur2)))),
                                                    'verticaux')
-                        return résultat
                     except QuoridorError:
                         pass
             else:
@@ -195,7 +193,6 @@ class Quoridor:
                                                                  zip(pos2,
                                                                      (1, -1 * (2 - joueur2))))),
                                                'verticaux')
-                    return résultat
                 # Si on ne peut pas placer de mur à cet  endroit, on deplace notre pion
                 except QuoridorError:
                     try:
@@ -203,11 +200,8 @@ class Quoridor:
                                                                      zip(pos2,
                                                                          (1, 1 - joueur2)))),
                                                    'verticaux')
-                        return résultat
                     except QuoridorError:
                         pass
-        # Si on ne peut pas placer de mur on deplace
-        résultat = self.déplacer_jeton(joueur, nx.shortest_path(graphe, pos1, f'B{joueur}')[1])
         return résultat
     def jouer_coup(self, joueur):
         """Méthode qui joue un coup automatique"""
